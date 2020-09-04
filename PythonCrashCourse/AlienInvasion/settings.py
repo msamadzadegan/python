79902b11af88da0efaa34f0dd23172fc673e4934
msamad <samadzadegan@outlook.com>
2020-09-04 22:55:52 +0430

    Create Setting class

---
+++ PythonCrashCourse/AlienInvasion/settings.py
@@ -0,0 +1,10 @@
+class Settings:
+	"""A class to store all settings for Alien Invasion"""
+
+	def __init__(self):
+		"""Initialize the game settings"""
+		#screen settings
+		self.screen_width = 800
+		self.screen_height = 600
+		self.bg_color = (230, 230, 255)
+		
